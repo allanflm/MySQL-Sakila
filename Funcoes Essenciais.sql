26d107bd30f2b026ff7a5c0f6da37adc39046437
Allan <allanfelipedk@gmail.com>
2025-12-22 11:16:45 -0300

    Funcoes Essenciais

---
+++ Funcoes Essenciais.sql
@@ -0,0 +1,10 @@
+SELECT LOCATE('o', 'Carros');
+
+SELECT LCASE('Carro'); -- lower case
+SELECT UCASE('carro'); -- upper case
+
+SELECT LENGTH('Carros');
+
+select repeat('carro', 4);
+
+-- https://www.w3schools.com/mysql/mysql_ref_functions.asp
